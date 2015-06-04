70a947d2695601fefa7ff15e48d10e5cea73a779
Brendan McElligott <brendan.mcelligott@gmail.com>
2015-06-04 12:48:08 -0700

    Update main example listener

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Shipped golang sample</h1><div>Hello World!!</div>")
+    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
 }
 
 func main() {
     http.HandleFunc("/", defaultHandler)
+    fmt.Println("Example app listening at http://localhost:8888")
     http.ListenAndServe(":8888", nil)
 }
